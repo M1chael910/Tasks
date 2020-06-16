09087de23932e56275f22d388c12c49c40824a76
Michael Murphy <mpmurphy1432@gmail.com>
2020-06-15 19:48:50 -0500

    Initial Commit

---
+++ Tasks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tasks
+//
+//  Created by Michael  Murphy on 6/15/20.
+//  Copyright © 2020 Michael  Murphy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
